05e594db554363db588b96b0acdd234694aa2206
raniaw@web.de <raniaw@web.de>
2022-11-01 16:08:57 +0100

    Testing react-scratch

---
+++ src/login/login.test.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { act } from 'react-dom/test-utils';
+import { Login } from './login';
+
+
+describe('Login component tests', () => {
+    let container: HTMLDivElement;
+    beforeEach(() => {
+        container = document.createElement('div');
+        document.body.appendChild(container);
+    })
+    afterEach(() => {
+        document.body.removeChild(container);
+        container = null;
+    });
+
+    it('can render login', () => {
+        act(() => {
+            ReactDOM.render(<Login />, container);
+        });
+    });
+});
